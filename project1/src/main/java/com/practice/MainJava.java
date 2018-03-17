00baa83cbc2506e54cec722420988e71bad8e3f8
Shuvajit Dutta <shuvajitduttawork@gmail.com>
2018-03-17 22:09:54 +0530

    github hook testing...MainJava.java modified

--- project1/src/main/java/com/practice/MainJava.java
+++ project1/src/main/java/com/practice/MainJava.java
@@ -5,5 +5,6 @@ public class MainJava {
 	public static void main() {
 		HelloWorld hwobj = new HelloWorld();
 		System.out.println(hwobj.Message());
+		System.out.println("I am github hook!!!");
 	}
 }
