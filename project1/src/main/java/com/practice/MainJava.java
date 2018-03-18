ccf522bead9e418f20867a00092bb1763a5b9f6d
Shuvajit Dutta <shuvajitduttawork@gmail.com>
2018-03-18 10:41:54 +0530

    webhhok testing...2nd attempt

--- project1/src/main/java/com/practice/MainJava.java
+++ project1/src/main/java/com/practice/MainJava.java
@@ -6,5 +6,6 @@ public class MainJava {
 		HelloWorld hwobj = new HelloWorld();
 		System.out.println(hwobj.Message());
 		System.out.println("Testing merge conflicts");
+		System.out.println("Testing webhook...2nd try");
 	}
 }
